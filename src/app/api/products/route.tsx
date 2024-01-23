8c4c2edecec3559fc374ce6350c188cf0c799387
Wei Han <87291797+xypheral@users.noreply.github.com>
2024-01-24 02:00:28 +0800

    updates

--- src/app/api/products/route.tsx
+++ src/app/api/products/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../../../prisma/client";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Milk', price: 2.5 },
-        { id: 2, name: 'Chocolate', price: 3.5 },
-    ])
+export async function GET(request: NextRequest) {
+    const products = await prisma.product.findMany();
+    return NextResponse.json(products);
+
 }
 
 export async function POST(request: NextRequest) {
@@ -13,5 +13,13 @@ export async function POST(request: NextRequest) {
     const validation = schema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
-    return NextResponse.json({ id: 10, name: body.name, price: body.price }, { status: 201 });
+
+    const newProduct = await prisma.product.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+
+    return NextResponse.json(newProduct,);
 }
